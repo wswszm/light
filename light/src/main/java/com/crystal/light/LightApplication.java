da149f48655e276a166a630c543f63ff516a2a18
crystalZhao <445655172@qq.com>
2019-04-23 16:09:18 +0800

    #Create Support Schedule Task , Create Schedule Task Sample

--- light/src/main/java/com/crystal/light/LightApplication.java
+++ light/src/main/java/com/crystal/light/LightApplication.java
@@ -5,11 +5,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+
 @SpringBootApplication
+//Mybatis包扫描
 @MapperScan("com.crystal.light.dao")
+//开启事务
 @EnableTransactionManagement
+//开启定时任务
+@EnableScheduling
 public class LightApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(LightApplication.class, args);
---
+++ light/src/main/java/com/crystal/light/schedule/ScheduleTask.java
@@ -0,0 +1,18 @@
+package com.crystal.light.schedule;
+
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author crystalzhao
+ * @version V1.0
+ * @date 2019/4/23
+ */
+@Component
+public class ScheduleTask {
+    @Scheduled(cron = "0/5 * * * * ?")
+    public void test(){
+        System.out.println("#############################");
+    }
+}
